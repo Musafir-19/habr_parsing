525d59812e3f5c487f87f119596b85c763db2f2a
Musafir-19 <rkhunkarov@mail.ru>
2021-12-03 22:41:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Парсер
+## Проект не закончен
+
+### Установите Python версии 3.6 и выше
+#### Зависимости
+
+#### Установите зависимости из файла requiremets.txt
+#### Команда для Windows `pip install -r requirements.txt`
+#### Команда для MacOS и Linux `pip3 install -r requirements.txt`
+
+### Запуск 
+#### Для запуска на Windows `python .\scrap.py`
+#### Для запуска на MacOs или Linix `python3 .\scrap.py`
---
+++ scrap.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+url = 'https://habr.com/ru/top/daily/'
+
+res = requests.get(url)
+
+habr = []
+
+if res.status_code == 200:
+    soup = BS(res.content, 'html.parser')
+    div = soup.find('div', class_='tm-articles-list')
+    articles = div.find_all('article', class_='tm-articles-list__item')
+    for item in articles:
+        url = 'https://habr.com' + item.h2.a['href']
+        title = item.h2.text
+        author = item.find(
+            'span', class_='tm-user-info tm-article-snippet__author').text.strip()
+        time = item.find(
+            'span', class_='tm-article-snippet__datetime-published').text
+        comments = item.find(
+            'span', class_='tm-article-comments-counter-link__value').text.strip()
+        habr.append({'url': url, 'title': title, 'author': author,
+                    'time': time, 'comments': comments})
+        print(comments)
+
+
+with open('habrScrap.json', 'w', encoding='utf-8') as f:
+    json.dump(habr, f, indent=4, ensure_ascii=False)
